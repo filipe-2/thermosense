fbe777d5725ec956a61485f27df64438f5be8696
Carlos Filipe <carlos.filipe.ramos.12>
2024-04-12 16:49:41 -0300

    refactor: Move home.js to screens folder

--- App.js
+++ App.js
@@ -1,32 +1,7 @@
-import { Text, View, ImageBackground } from 'react-native';
-import { styles } from './styles/home';
-import { useEffect, useState } from 'react';
-import { db, ref, onValue } from './firebase';
+import Home from './screens/home';
 
 export default function App() {
-  const [temperature, setTemperature] = useState(-15);
-  const [humidity, setHumidity] = useState(69);
-
-  useEffect(() => {
-    const data = ref(db);
-
-    onValue(data, snapshot => {
-      setTemperature(snapshot.val().temperature);
-      setHumidity(snapshot.val().humidity);
-    });
-  }, [db]);
-
   return (
-    <ImageBackground style={styles.wrapper} source={require('./assets/bg.jpg')}>
-      <View style={styles.temperatureWrapper}>
-        <Text style={styles.text}>TEMPERATURE</Text>
-        <Text style={[styles.text, styles.temperatureText]}>{temperature}°C</Text>
-      </View>
-
-      <View style={styles.temperatureWrapper}>
-        <Text style={styles.text}>HUMIDITY</Text>
-        <Text style={[styles.text, styles.temperatureText]}>{humidity}%</Text>
-      </View>
-    </ImageBackground>
+    <Home />
   );
 }
---
+++ screens/home.js
@@ -0,0 +1,32 @@
+import { Text, View, ImageBackground } from 'react-native';
+import { styles } from '../styles/home';
+import { useEffect, useState } from 'react';
+import { db, ref, onValue } from '../firebase';
+
+export default function Home() {
+    const [temperature, setTemperature] = useState(0);
+    const [humidity, setHumidity] = useState(0);
+
+    useEffect(() => {
+        const data = ref(db);
+
+        onValue(data, snapshot => {
+            setTemperature(snapshot.val().temperature);
+            setHumidity(snapshot.val().humidity);
+        });
+    }, [db]);
+
+    return (
+        <ImageBackground style={styles.wrapper} source={require('../assets/bg.jpg')}>
+            <View style={styles.temperatureWrapper}>
+                <Text style={styles.text}>TEMPERATURE</Text>
+                <Text style={[styles.text, styles.temperatureText]}>{temperature}°C</Text>
+            </View>
+
+            <View style={styles.temperatureWrapper}>
+                <Text style={styles.text}>HUMIDITY</Text>
+                <Text style={[styles.text, styles.temperatureText]}>{humidity}%</Text>
+            </View>
+        </ImageBackground>
+    );
+}
