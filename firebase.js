36adf2de4976c5354d4299e03b7f9aa7cd699481
Carlos Filipe <carlosfilipesoaresramos2001@gmail.com>
2024-04-14 12:04:29 -0300

    refactor: Move firebase.js to .gitignore

--- firebase.js
+++ firebase.js
@@ -4,12 +4,12 @@ import 'firebase/database';
 
 // Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyALbZa77M-LI3gOfXlp6iplXjC4SyMGujA",
-    authDomain: "thermo-sense.firebaseapp.com",
-    projectId: "thermo-sense",
-    storageBucket: "thermo-sense.appspot.com",
-    messagingSenderId: "1059816778862",
-    appId: "1:1059816778862:web:8b7ef9e4f7f283d4b94a7f"
+    apiKey: "AIzaSyA1FoizL5m7eup3flmzvUjdU763MmYZ014",
+    authDomain: "thermo-sense-app.firebaseapp.com",
+    projectId: "thermo-sense-app",
+    storageBucket: "thermo-sense-app.appspot.com",
+    messagingSenderId: "110348919644",
+    appId: "1:110348919644:web:a5e303c8bdc82fde565799"
 };
 
 // Initialize firebase
