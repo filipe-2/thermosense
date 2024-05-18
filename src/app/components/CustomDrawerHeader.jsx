a2442458d98eff5411531da47e52edc826654796
Carlos Filipe <carlosfilipesoaresramos2001@gmail.com>
2024-05-18 19:56:21 -0300

    feat: Adicionar imagem de fundo no header do drawer

--- src/app/components/CustomDrawerHeader.jsx
+++ src/app/components/CustomDrawerHeader.jsx
@@ -1,4 +1,4 @@
-import { View, Text, TouchableOpacity, Image } from 'react-native';
+import { View, Text, TouchableOpacity, Image, ImageBackground } from 'react-native';
 
 // Icons
 import Icon from 'react-native-vector-icons/FontAwesome';
@@ -8,10 +8,10 @@ import { colors } from '../styles/global/customStyles';
 
 export default function CustomDrawerHeader() {
     return (
-        <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingVertical: 15, paddingHorizontal: 20, height: 80, backgroundColor: colors.clr_3, borderBottomWidth: 2, borderColor: colors.clr_1 }}>
-            <View style={{ flexDirection: 'row', gap: 15 }}>
-                <TouchableOpacity>
-                    <Image source={require('../../../assets/profile-img.jpg')} style={{ width: 25, height: 25 }} />
+        <ImageBackground source={require('../../../assets/auth-bg.jpg')} style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingTop: 30, paddingHorizontal: 20, height: 100, backgroundColor: colors.clr_7, borderBottomWidth: 5, borderColor: colors.clr_1 }}>
+            <View style={{ flexDirection: 'row', gap: 15, justifyContent: 'center', alignItems: 'center' }}>
+                <TouchableOpacity style={{ justifyContent: 'center', alignItems: 'center', width: 35, height: 35, borderRadius: 18.25, borderWidth: 1, borderColor: colors.clr_1 }}>
+                    <Image source={require('../../../assets/profile-img.jpg')} style={{ width: 25, height: 25, borderRadius: 12.25 }} />
                 </TouchableOpacity>
 
                 <TouchableOpacity>
@@ -19,16 +19,13 @@ export default function CustomDrawerHeader() {
                 </TouchableOpacity>
             </View>
 
-            <View pointerEvents="none" style={{ position: 'absolute', top: 0, bottom: 0, left: 0, right: 0, justifyContent: 'center', alignItems: 'center' }}>
-                <Image source={require('../../../assets/logo.png')} style={{
-                    width: 50, height: 50
-
-                }} />
+            <View pointerEvents="none" style={{ position: 'absolute', top: 0, bottom: 0, left: 0, right: 0, justifyContent: 'center', alignItems: 'center', paddingTop: 30 }}>
+                <Image source={require('../../../assets/logo.png')} style={{ width: 50, height: 50 }} />
             </View>
 
             <TouchableOpacity>
                 <Icon name='bars' color={colors.clr_1} size={25} />
             </TouchableOpacity>
-        </View>
+        </ImageBackground>
     );
 }
